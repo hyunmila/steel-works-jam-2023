47691c8f4c90b649cdc19ce719be35478d4ce69a
Igor Urbanik <igorurbanik23@gmail.com>
2023-02-24 19:24:03 +0100

    Added modular window

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
+from pygame.math import Vector2 as Vec2
 from colors import COLOR
+
+pygame.transform
 
 pygame.init()
 
-WIDTH, HEIGHT = 800, 800
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
+WIDTH, HEIGHT = 600, 600
+CENTER = pygame.math.Vector2(300, 300)
+__screen = pygame.display.set_mode([WIDTH, HEIGHT], pygame.RESIZABLE)
+screen = __screen.copy()
+
 pygame.display.set_caption("SteelWorksJam 2023")
 
 running = True
@@ -15,6 +21,7 @@ dt = clock.tick(60)
 ticks = 0
 
 while running:
+    screen.fill(COLOR.BLACK)
     dt = clock.tick(60)
     ticks += dt
 
@@ -24,8 +31,11 @@ while running:
 
     pressed_keys = pygame.key.get_pressed()
 
+    pygame.draw.circle(screen, COLOR.WHITE, Vec2(200, 200), 50)
+
     if pressed_keys[pygame.K_w]:
         print(f"dt = {dt}")
 
-    screen.fill(COLOR.BLACK)
+    __scaled = pygame.transform.scale(screen, (__screen.get_width(), __screen.get_height()))
+    __screen.blit(__scaled, (0, 0))
     pygame.display.flip()
